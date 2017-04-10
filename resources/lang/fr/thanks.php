edbeccd147bd2760fe18d3d7af3c942ab01428eb
Sergey Priakhin <sergey.priakhin@gmail.com>
2017-04-10 17:00:52 +0300

    fix

--- resources/lang/fr/thanks.php
+++ resources/lang/fr/thanks.php
@@ -6,19 +6,19 @@ Grâce à vous tous qui avez pris une part dans la réalisation du projet et à 
 </p>
 <div style="margin-top: 20px;">
 <p>Le logo, certains éléments graphiques et textes ont été réalisés par:</p>
-<p><a href="http://www.graphiclanguage.ca" target="_blank">www.graphiclanguage.ca</a></p>
+<p><a href="http://www.graphiclanguage.ca" class="tanks-popup-link" target="_blank">www.graphiclanguage.ca</a></p>
 
 <p>Les photos de Stéphane ont été prises par Joseph Hall: </p>
-<p><a href="http://www.josephhallphotography.com" target="_blank">www.josephhallphotography.com</a></p>
+<p><a href="http://www.josephhallphotography.com" class="tanks-popup-link" target="_blank">www.josephhallphotography.com</a></p>
 
 <p>Les autres photos proviennent de ces belles initiatives:</p>
-<p><a href="http://www.unsplash.com"  target="_blank"> www.unsplash.com</a></p>
+<p><a href="http://www.unsplash.com" class="tanks-popup-link" target="_blank"> www.unsplash.com</a></p>
 
 <p>et</p>
-<p><a href="http://www.rawpixel.com" target="_blank">www.rawpixel.com </a></p>
+<p><a href="http://www.rawpixel.com" class="tanks-popup-link" target="_blank">www.rawpixel.com </a></p>
 
 <p>Les enregistrements ont été réalisés ici :</p>
-<p><a href="http://www.templestudios.com" target="_blank">www.templestudios.com</a></p>
+<p><a href="http://www.templestudios.com" class="tanks-popup-link" target="_blank">www.templestudios.com</a></p>
 
 <p>Nous voudrions tout spécialement remercier:</p>
 <p>
--- resources/views/site/presse.blade.php
+++ resources/views/site/presse.blade.php
@@ -8,6 +8,6 @@
     <div class="press-text">
         <p>Vous trouverez ici notre dossier de presse ainsi que les photos et logos de Turbulence Zéro.</p>
         <p>Merci de bien vouloir les diffuser, nous vous en remercions vivement ! </p>
-        </p>
+        <p><a href="/src/download/pressPageZip.zip" style="padding: 10px 0;display: block;">Cliquez ici pour télécharger.</a></p>
     </div>
 </div>
