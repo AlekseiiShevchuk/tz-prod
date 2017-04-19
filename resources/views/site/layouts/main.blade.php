447500109c32b2507f7ea31bbc436fed3525acc9
Aleksey Shevchuk <shevchuk.a@gmail.com>
2017-04-19 17:41:01 +0300

    add counter

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -147,6 +147,22 @@ Venez découvrir gratuitement la façon la plus facile d’appréhender la médi
 @include('site.popups.privee')
 
 @include('site.popups.tanks')
-
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+    var sc_project=11319960;
+    var sc_invisible=1;
+    var sc_security="2645de8a";
+    var scJsHost = (("https:" == document.location.protocol) ?
+        "https://secure." : "http://www.");
+    document.write("<sc"+"ript type='text/javascript' src='" +
+        scJsHost+
+        "statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="web stats"
+                                      href="http://statcounter.com/" target="_blank"><img
+                    class="statcounter"
+                    src="//c.statcounter.com/11319960/0/2645de8a/1/" alt="web
+stats"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
 </body>
 </html>
