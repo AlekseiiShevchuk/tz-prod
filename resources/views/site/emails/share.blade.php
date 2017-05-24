6e2be561441f17cba0e906f118ed4196e7257ade
Olexiy <shevchuka@gmail.com>
2017-05-24 12:55:59 +0300

    share email fix layout

--- resources/views/site/emails/share.blade.php
+++ resources/views/site/emails/share.blade.php
@@ -2,13 +2,13 @@
 
 <p>{{$name}} vous invite à découvrir Turbulence Zéro gratuitement.</p>
 
-<p>Turbulence Zéro est un site de découverte de la méditation.
+<p>Turbulence Zéro est un site de découverte de la méditation.<br>
 Cette pratique millénaire nous aide à calmer notre esprit en quelques minutes, n’importe où, n’importe quand.</p>
 
 <p><a href="https://turbulencezero.com/login">Cliquez sur ici pour en profiter!</a></p>
 
-A bientôt!
-L’équipe de Turbulence Zéro.
+A bientôt!<br>
+L’équipe de Turbulence Zéro.<br><br>
 
-Si vous éprouvez des difficultés en cliquant sur le lien, copier et coller celui-ci dans votre navigateur:
+Si vous éprouvez des difficultés en cliquant sur le lien, copier et coller celui-ci dans votre navigateur:<br>
 https://turbulencezero.com/login
