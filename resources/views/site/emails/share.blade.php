b4912fe17f0a55ab0d46a24e55ad25af29c7df21
Olexiy <shevchuka@gmail.com>
2017-06-27 17:52:53 +0300

    fix invite email

--- resources/views/site/emails/share.blade.php
+++ resources/views/site/emails/share.blade.php
@@ -1,14 +1,16 @@
-<p>Hello.</p>
+<p>Bonjour.</p>
 
-<p>{{$name}} vous invite à découvrir Turbulence Zéro gratuitement.</p>
+<p>Quelqu'un vous invite à découvrir Turbulence Zéro gratuitement.</p>
 
 <p>Turbulence Zéro est un site de découverte de la méditation.<br>
-Cette pratique millénaire nous aide à calmer notre esprit en quelques minutes, n’importe où, n’importe quand.</p>
+    Cette pratique millénaire nous aide à calmer notre esprit en quelques minutes, n’importe où, n’importe quand.</p>
 
-<p><a href="https://turbulencezero.com/login">Cliquez sur ici pour en profiter!</a></p>
+<p>Cliquez sur ici pour en profiter!</p>
 
+<p>
 A bientôt!<br>
-L’équipe de Turbulence Zéro.<br><br>
-
+L’équipe de Turbulence Zéro.
+</p>
+<br>
 Si vous éprouvez des difficultés en cliquant sur le lien, copier et coller celui-ci dans votre navigateur:<br>
 https://turbulencezero.com/login
